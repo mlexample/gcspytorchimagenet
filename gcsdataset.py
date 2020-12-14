97100a0127e8fe88f8d629928272001a912978fd
shanemhansen <shanemhansen@google.com>
2020-12-14 23:52:12 +0000

    Autopep8

--- gcsdataset.py
+++ gcsdataset.py
@@ -14,23 +14,26 @@ import torchvision.transforms as transforms
 import torch_xla.utils.gcsfs
 
 _client = None
+
+
 def get_client():
     global _client
     if _client is None:
         _client = Client()
     return _client
 
+
 def make_dataset(
         directory: str,
         paths: List[str],
-        extensions: Optional[Tuple[str, ...]]=None,
-) -> Tuple[List[Tuple[str, int]], Dict[bytes,int]]:
+        extensions: Optional[Tuple[str, ...]] = None,
+) -> Tuple[List[Tuple[str, int]], Dict[bytes, int]]:
     """
     Map folder+classnames into list of (imagepath, class_index). Requires potentially expensive glob of
     virtual filesystem. For large object store datasets it's recommended to cache this.
     """
     instances = []
-    counter=0
+    counter = 0
     if extensions is None:
         extensions = ()
     # make it easier to directly match on result of str.split
@@ -44,21 +47,25 @@ def make_dataset(
         if fname.split('.')[-1].lower() not in extSet:
             continue
         if potentialclass not in classes_to_idx:
-            classes_to_idx[potentialclass]=counter
-            counter+=1
+            classes_to_idx[potentialclass] = counter
+            counter += 1
         instances.append((path, classes_to_idx[potentialclass]))
     return instances, classes_to_idx
+
 
 def write_bytes(path: str, data: str):
     with open(path, 'w') as f:
         f.write(data)
+
+
 def read_bytes(path: str):
     try:
         with open(path, 'rb') as f:
             return f.read()
     except FileNotFoundError:
         raise NotFoundException()
-    
+
+
 def gcs_write_bytes(path: str, data: str):
     # skip caching if local
     blob = Blob.from_string(path)
@@ -66,84 +73,95 @@ def gcs_write_bytes(path: str, data: str):
     blob.bucket._client = get_client()
     blob.upload_from_string(data)
 
+
 def gcs_read_bytes(path: str):
     try:
         f = io.BytesIO()
         get_client().download_blob_to_file(path, f)
         f.seek(0)
         return f.read()
-    except Exception: # TODO narrow this
+    except Exception:  # TODO narrow this
         raise NotFoundException()
+
 
 class NotFoundException(Exception):
     pass
+
+
 class ImageFolder(VisionDataset):
     def __init__(
-            self,
-            root: str,
-            index_path: Optional[str] = None,
-            extensions: Optional[Tuple[str, ...]] = None,
-            transform: Optional[Callable] = None,
-            target_transform: Optional[Callable] = None,
-            is_valid_file: Optional[Callable[[str], bool]] = None,
-) -> None:
-      super(ImageFolder, self).__init__(root, transform=transform,
+        self,
+        root: str,
+        index_path: Optional[str] = None,
+        extensions: Optional[Tuple[str, ...]] = None,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        is_valid_file: Optional[Callable[[str], bool]] = None,
+    ) -> None:
+        super(ImageFolder, self).__init__(root, transform=transform,
                                           target_transform=target_transform)
-      
-      load_from_cache = True
-      samples = None
-      paths = None
-      self.write_cb = write_bytes
-      self.read_cb = read_bytes
-      if index_path is not None and "gs://" in index_path:
-          self.write_cb = gcs_write_bytes
-          self.read_cb = gcs_read_bytes
-      if index_path is not None:
-          try:
-              paths = json.loads(self.read_cb(index_path))
-          except NotFoundException:
-              pass
-      if paths is None:         
-          load_from_cache = False
-          # note: relying on private api to avoid some extra stat calls.
-          paths = list(torch_xla._XLAC._xla_tffs_list(os.path.join(self.root, "*", "*.JPEG"))) # pytype: disable=module-attr
-          paths.sort()
-      samples, classes_to_idx = make_dataset(self.root, paths, torchvision.datasets.folder.IMG_EXTENSIONS)  # pytype: disable=module-attr
-      
-      if len(samples) == 0:          
-        msg = "Found 0 files in subfolders of: {}\n".format(self.root)
-        if extensions is not None:
-          msg += "Supported extensions are: {}".format(",".join(extensions))
-          raise RuntimeError(msg)
-      classes = list(classes_to_idx.keys())
-      classes.sort()
-      self.classes = classes
-      self.class_to_idx = classes_to_idx
-      self.samples = samples
-      self.targets = [s[1] for s in samples]
-      self.imgs = self.samples
-      if index_path is not None and not load_from_cache:
-          self._cache_index(index_path, paths)
-      self._buf = io.BytesIO()
+
+        load_from_cache = True
+        samples = None
+        paths = None
+        self.write_cb = write_bytes
+        self.read_cb = read_bytes
+        if index_path is not None and "gs://" in index_path:
+            self.write_cb = gcs_write_bytes
+            self.read_cb = gcs_read_bytes
+        if index_path is not None:
+            try:
+                paths = json.loads(self.read_cb(index_path))
+            except NotFoundException:
+                pass
+        if paths is None:
+            load_from_cache = False
+            # note: relying on private api to avoid some extra stat calls.
+            paths = list(torch_xla._XLAC._xla_tffs_list(os.path.join(
+                self.root, "*", "*.JPEG")))  # pytype: disable=module-attr
+            paths.sort()
+        samples, classes_to_idx = make_dataset(
+            self.root, paths, torchvision.datasets.folder.IMG_EXTENSIONS)  # pytype: disable=module-attr
+
+        if len(samples) == 0:
+            msg = "Found 0 files in subfolders of: {}\n".format(self.root)
+            if extensions is not None:
+                msg += "Supported extensions are: {}".format(
+                    ",".join(extensions))
+                raise RuntimeError(msg)
+        classes = list(classes_to_idx.keys())
+        classes.sort()
+        self.classes = classes
+        self.class_to_idx = classes_to_idx
+        self.samples = samples
+        self.targets = [s[1] for s in samples]
+        self.imgs = self.samples
+        if index_path is not None and not load_from_cache:
+            self._cache_index(index_path, paths)
+        self._buf = io.BytesIO()
 
     def loader(self, uri):
-      f = self._buf
-      f.seek(0)
-      f.write(self.read_cb(uri))
-      img = Image.open(f).convert('RGB')
-      return img
+        f = self._buf
+        f.seek(0)
+        f.write(self.read_cb(uri))
+        img = Image.open(f).convert('RGB')
+        return img
+
     def _cache_index(self, fname: str, paths: List[str]) -> None:
         self.write_cb(fname, json.dumps(paths))
+
     def __len__(self):
-      return len(self.samples)
+        return len(self.samples)
+
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
-      path, target = self.samples[index]
-      sample = self.loader(path)
-      if self.transform is not None:
-        sample = self.transform(sample)
-      if self.target_transform is not None:
-        target = self.target_transform(target)      
-      return sample, target
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return sample, target
+
 
 if __name__ == "__main__":
     import sys
@@ -152,7 +170,7 @@ if __name__ == "__main__":
     img_dim = 224
     if 'IMAGE_DIR' not in os.environ:
         raise Exception("IMAGE_DIR env variable is required")
-    directory=os.environ['IMAGE_DIR']
+    directory = os.environ['IMAGE_DIR']
     train_dataset = ImageFolder(
         root=directory+"/train",
         index_path=directory+'/imagenetindex.json',
@@ -187,6 +205,6 @@ if __name__ == "__main__":
         print(dataset[0])
         for i in range(0, 100):
             sample = dataset[i]
-            if i%100 == 0:
+            if i % 100 == 0:
                 avg = (i+1)/(time.time()-t0)
-                print("Loaded {} samples. Average {} samples/s".format( (i+1), avg))
+                print("Loaded {} samples. Average {} samples/s".format((i+1), avg))
--- test_gcsdataset.py
+++ test_gcsdataset.py
@@ -9,66 +9,76 @@ import gcsdataset
 # A minimally small valid jpeg file
 img = b'\xff\xd8\xff\xdb\x00C\x00\x03\x02\x02\x02\x02\x02\x03\x02\x02\x02\x03\x03\x03\x03\x04\x06\x04\x04\x04\x04\x04\x08\x06\x06\x05\x06\t\x08\n\n\t\x08\t\t\n\x0c\x0f\x0c\n\x0b\x0e\x0b\t\t\r\x11\r\x0e\x0f\x10\x10\x11\x10\n\x0c\x12\x13\x12\x10\x13\x0f\x10\x10\x10\xff\xc9\x00\x0b\x08\x00\x01\x00\x01\x01\x01\x11\x00\xff\xcc\x00\x06\x00\x10\x10\x05\xff\xda\x00\x08\x01\x01\x00\x00?\x00\xd2\xcf \xff\xd9'
 
+
 class TestGCSDataset(unittest.TestCase):
     def setUp(self):
         self.dirs = ["a", "b"]
         self.directory = tempfile.TemporaryDirectory()
-        self.dirname=self.directory.name
+        self.dirname = self.directory.name
         self.count = 0
         self.paths = []
         for key in self.dirs:
             # create the dir/${label}/file.JPEG structure
             path = os.path.join(self.dirname, key)
             os.makedirs(path)
-            self.count +=1
+            self.count += 1
             fname = os.path.join(path, "file.JPEG")
             with open(fname, "wb") as f:
                 f.write(img)
             self.paths.append(fname)
+
     def tearDown(self):
-        self.directory.cleanup()        
+        self.directory.cleanup()
+
     def test_make_dataset(self):
-        ds = gcsdataset.make_dataset(self.directory.name, self.paths, extensions=(".JPEG",))
-        self.assertEqual(len(ds), self.count, "Expected {} items got {}".format(self.count, len(ds)))
+        ds = gcsdataset.make_dataset(
+            self.directory.name, self.paths, extensions=(".JPEG",))
+        self.assertEqual(len(ds), self.count,
+                         "Expected {} items got {}".format(self.count, len(ds)))
 
 
 class TestImageFolder(unittest.TestCase):
     def setUp(self):
-        self.classes_to_idx={b"a":0, b"b": 1}
+        self.classes_to_idx = {b"a": 0, b"b": 1}
         self.directory = tempfile.TemporaryDirectory()
-        self.dirname=self.directory.name.encode('utf-8')
+        self.dirname = self.directory.name.encode('utf-8')
         self.count = 0
         self.paths = []
         for key in self.classes_to_idx.keys():
             # create the dir/${label}/file.JPEG structure
             path = os.path.join(self.dirname, key)
             os.makedirs(path)
-            self.count +=1
+            self.count += 1
             fname = os.path.join(path, b"file.JPEG")
             with open(fname, "wb") as f:
                 f.write(img)
             self.paths.append(fname.decode('utf-8'))
         self.paths.sort()
+
     def tearDown(self):
         self.directory.cleanup()
+
     def test_image_folder(self):
         ds = gcsdataset.ImageFolder(root=self.directory.name)
         vals = list(ds)
-        self.assertEqual(len(vals), self.count, "Expected {} items got {}".format(self.count, len(vals)))
+        self.assertEqual(len(vals), self.count,
+                         "Expected {} items got {}".format(self.count, len(vals)))
+
     def test_index_cache(self):
         index_path = os.path.join(self.directory.name, "vals.json")
-        ds = gcsdataset.ImageFolder(root=self.directory.name, index_path=index_path)
+        ds = gcsdataset.ImageFolder(
+            root=self.directory.name, index_path=index_path)
         self.assertTrue(os.path.exists(index_path), "Index path exists")
         # Bad test practice here: verify cache data is expected structure
         with open(index_path) as f:
             paths = json.loads(f.read())
         self.assertEqual(paths, self.paths)
-        
-        
+
 
 class TestTrainScript(unittest.TestCase):
     def test_import_train_script(self):
         import test_train_mp_imagenet
-            
+
+
 if __name__ == '__main__':
     unittest.main()
